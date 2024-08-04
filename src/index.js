74466546a360ab20c1719d78a203a5e65368e306
TerebinovSergey <neznacomec94@gmail.com>
2024-08-05 00:28:37 +0400

    fix: remove button print, save html and udpate readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import turnOnRippleAnimation from './scripts/ripple-animation.js';
 import { saveTextContent, loadTextContent } from './scripts/cv-store.js';
-import { saveViaHtml2pdf, saveAsHtml } from './scripts/save-cv.js';
+import { saveViaHtml2pdf } from './scripts/save-cv.js';
 
 window.addEventListener('unload', saveTextContent);
 
@@ -8,13 +8,5 @@ document.addEventListener('DOMContentLoaded', loadTextContent);
 
 const saveBtn = document.querySelector('.save-btn');
 if (saveBtn) saveBtn.addEventListener('click', saveViaHtml2pdf);
-
-const printBtn = document.querySelector('.print-btn');
-if (printBtn) {
-  printBtn.addEventListener('click', () => setTimeout(() => window.print()));
-}
-
-const saveHtmlBtn = document.querySelector('.save-html-btn');
-if (saveHtmlBtn) saveHtmlBtn.addEventListener('click', saveAsHtml);
 
 turnOnRippleAnimation();
--- src/scripts/save-cv.js
+++ src/scripts/save-cv.js
@@ -9,18 +9,3 @@ export function saveViaHtml2pdf() {
   // eslint-disable-next-line no-undef
   html2pdf().from(wrapper).save('resume.pdf');
 }
-
-export function saveAsHtml() {
-  const footer = document.querySelector('.footer');
-  footer.style.display = 'none';
-  const blob = new Blob([document.body.outerHTML], { type: 'text/html' });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'resume.html';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  URL.revokeObjectURL(url);
-  footer.style.display = 'block';
-}
